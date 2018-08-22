1525dfdbca01dcd36fa96764e332bc896bf4d58a
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-22 21:00:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
-  end
+   get '/food_form' do
+      erb :food_form
+   end
 
-  # Add your post route and action below
+   # Add your post route and action below
 
 end
